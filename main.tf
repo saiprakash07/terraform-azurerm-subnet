1fbde803057ba48bf175fbefd16c62147be6417e
saiprakash07 <88754277+saiprakash07@users.noreply.github.com>
2022-04-19 14:46:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,7 @@
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = "rg1"
+  virtual_network_name = "vnet1"
+  address_prefixes     = ["10.0.1.0/24"]
 
-
+}
